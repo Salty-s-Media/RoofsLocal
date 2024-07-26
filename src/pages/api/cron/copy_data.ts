38b67e934b33f59b7704527f63f4bd80ad9f9b18
rcong315 <richardcong635@gmail.com>
2024-07-26 12:07:40 -0700

    Copy leads

--- src/pages/api/cron/copy_data.ts
+++ src/pages/api/cron/copy_data.ts
@@ -8,7 +8,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient as PrismaClientProd } from '@prisma/client';
 import { PrismaClient as PrismaClientDev } from '@prisma-dev/client';
 
-// Initialize two Prisma Clients for two different databases
 const prismaProd = new PrismaClientProd();
 const prismaDev = new PrismaClientDev({ 
   datasources: { 
@@ -16,6 +15,55 @@ const prismaDev = new PrismaClientDev({
       url: process.env.POSTGRES_DEV_PRISMA_URL, 
       directUrl: process.env.POSTGRES_DEV_URL_NON_POOLING
 }}});
+
+async function copyContractors() {
+  const contractors = await prismaProd.contractor.findMany();
+  await prismaDev.contractor.createMany({
+    data: contractors,
+    skipDuplicates: true,
+  });
+}
+
+const HUBSPOT_SOURCE_API_KEY = process.env.HUBSPOT_API_KEY;
+const HUBSPOT_DEST_API_KEY = process.env.HUBSPOT_DEV_API_KEY;
+
+async function fetchContacts() {
+  const response = await fetch(
+    `https://api.hubapi.com/crm/v3/objects/contacts?hapikey=${HUBSPOT_SOURCE_API_KEY}`
+  );
+  return response.json();
+}
+
+async function createContact(contactData) {
+  const response = await fetch(
+    `https://api.hubapi.com/crm/v3/objects/contacts?hapikey=${HUBSPOT_DEST_API_KEY}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contactData),
+    }
+  );
+  return response.json();
+}
+
+async function copyLeads() {
+  const { results } = await fetchContacts();
+    const createPromises = results.map(contact => {
+      const contactData = {
+        properties: {
+          firstname: contact.properties.firstname,
+          lastname: contact.properties.lastname,
+          email: contact.properties.email,
+          phone: contact.properties.phone,
+          zip: contact.properties.zip,
+        },
+      };
+      return createContact(contactData);
+    });
+    await Promise.all(createPromises);
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (process.env.VERCEL_ENV !== 'production') {
@@ -26,12 +74,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   try {
-    const data = await prismaProd.contractor.findMany();
-
-    await prismaDev.contractor.createMany({
-      data,
-      skipDuplicates: true,
-    });
+    await copyContractors();
+    await copyLeads();
 
     res.status(200).json({ message: 'Data copied successfully' });
   } catch (error) {
