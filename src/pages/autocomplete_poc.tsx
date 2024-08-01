8b0b6584d50b7c6ee9f4b09a331d6254acab8042
rcong315 <richardcong635@gmail.com>
2024-07-31 22:40:38 -0700

    Place api

---
+++ src/pages/autocomplete_poc.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect } from 'react';
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
+
+const PlacesAutocomplete = () => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    callbackName: "initAutocomplete",
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 300,
+  });
+  const ref = useOnclickOutside(() => {
+    clearSuggestions();
+  });
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCRrEZcrzIK4iBN1aM8XI62OFLAbhXoreM&libraries=places&callback=initAutocomplete";
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = ({ description }) => () => {
+    setValue(description, false);
+    clearSuggestions();
+    getGeocode({ address: description }).then((results) => {
+      const { lat, lng } = getLatLng(results[0]);
+      console.log("📍 Coordinates: ", { lat, lng });
+    });
+  };
+
+  const renderSuggestions = () => data.map((suggestion) => {
+    const { place_id, structured_formatting: { main_text, secondary_text } } = suggestion;
+    return (
+      <li key={place_id} onClick={handleSelect(suggestion)}>
+        <strong>{main_text}</strong> <small>{secondary_text}</small>
+      </li>
+    );
+  });
+
+  return (
+    <div 
+      ref={ref} 
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh', // Use the full height of the viewport
+      }}
+    >
+      <input
+        value={value}
+        onChange={handleInput}
+        disabled={!ready}
+        placeholder="Where are you going?"
+        style={{
+          margin: '0 auto', // Center the input horizontally if needed
+          display: 'block', // Ensure the input is a block element for margin to take effect
+          color: 'black'
+        }}
+      />
+      {status === "OK" && <ul>{renderSuggestions()}</ul>}
+    </div>
+  );
+};
+
+export default PlacesAutocomplete;
