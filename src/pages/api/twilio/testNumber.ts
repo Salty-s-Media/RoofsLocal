72ef590b43878276d212e26eb1e2b35b15b5adac
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-08-11 09:22:51 -0700

    minor addition

--- src/pages/api/twilio/testNumber.ts
+++ src/pages/api/twilio/testNumber.ts
@@ -11,20 +11,20 @@ export default async function handler(
       process.env.TWILIO_AUTH_TOKEN
     );
 
+    let call: any = {};
+
     try {
-      const call = await client.calls.create({
+      call = await client.calls.create({
         from: "+987654321",
         to: "+123456789",
         url: "http://demo.twilio.com/docs/voice.xml",
       });
-
-      return res.status(200).json({ message: call });
     } catch (error) {
       console.error("Failed to create call:", error);
-      alert("Failed to create call.");
+      return res.status(500).json({ message: "Failed to create call" });
     }
 
-    return res.status(200).json({ message: "gg" });
+    return res.status(200).json({ message: call });
   } else {
     res.setHeader("Allow", ["POST"]);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
