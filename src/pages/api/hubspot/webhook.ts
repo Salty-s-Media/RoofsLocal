69d9dde7f3c5f4edea16a1da489194febcbdd03d
rcong315 <richardcong635@gmail.com>
2024-09-13 21:26:27 -0700

    Add webhook

---
+++ src/pages/api/hubspot/webhook.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { body } = req;
+    
+    console.log('Webhook received:', body);
+    
+    res.status(200).json({ message: 'Webhook received successfully' });
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
