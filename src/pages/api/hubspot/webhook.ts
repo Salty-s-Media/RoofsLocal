461c43aa6c0b9cd897ed299165d64106e38c66a5
rcong315 <richardcong635@gmail.com>
2024-09-16 21:17:13 -0700

    Migrate lead to other hubspot

--- src/pages/api/hubspot/webhook.ts
+++ src/pages/api/hubspot/webhook.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
+const CONTRACTOR_API_KEY = process.env.DEMO_HUBSPOT_API_KEY;
 
 interface WebhookData {
   total: number,
@@ -16,6 +17,10 @@ interface WebhookData {
         phone: string;
         plan: string | null;
         job_status: string | null;
+        createdate: string;
+        lastmodifieddate: string;
+        zip: string | null;
+        company: string | null;
       }
     }
   ]
@@ -24,67 +29,99 @@ interface WebhookData {
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { body } = req;
-    
+
     console.log('Webhook received:', body);
 
-    // body.map(async (data: WebhookData) => {
-
-      const postData = {
-        filterGroups: [
-          {
-            filters: [
-              {
-                propertyName: "hs_object_id",
-                operator: "EQ",
-                value: body.objectId,
-              },
-            ],
-          },
-        ],
-        properties: [
-          "id",
-          "hs_lead_status",
-          "firstname",
-          "lastname",
-          "email",
-          "phone",
-          "plan",
-          "job_status",
-        ],
-      };
-
-      try {
-        const hubspotResponse = await fetch(
-          "https://api.hubapi.com/crm/v3/objects/contacts/search",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${HUBSPOT_API_KEY}`,
+    const postData = {
+      filterGroups: [
+        {
+          filters: [
+            {
+              propertyName: "hs_object_id",
+              operator: "EQ",
+              value: body.objectId,
             },
-            body: JSON.stringify(postData),
-          }
-        );
+          ],
+        },
+      ],
+      properties: [
+        "id",
+        "hs_lead_status",
+        "firstname",
+        "lastname",
+        "email",
+        "phone",
+        // "plan",
+        // "job_status",
+        "contact_owner",
+        "zip"
+      ],
+    };
 
-        if (!hubspotResponse.ok) {
-          const errorBody = await hubspotResponse.text();
-          throw new Error(
-            `HubSpot API request failed with status ${hubspotResponse.status}: ${errorBody}`
-          );
+    try {
+      const hubspotResponse = await fetch(
+        "https://api.hubapi.com/crm/v3/objects/contacts/search",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${HUBSPOT_API_KEY}`,
+          },
+          body: JSON.stringify(postData),
         }
+      );
 
-        const data: WebhookData = await hubspotResponse.json();
-        const lead = data.results[0].properties;
-
-        console.log('New lead data from webhook: ', lead);
-      } catch (error) {
-        console.error(
-          `Error processing webhook data from HubSpot:`,
-          body.objectId, error
+      if (!hubspotResponse.ok) {
+        const errorBody = await hubspotResponse.text();
+        throw new Error(
+          `HubSpot API request failed with status ${hubspotResponse.status}: ${errorBody}`
         );
       }
-    // });
-    
+
+      const data: WebhookData = await hubspotResponse.json();
+      const lead = data.results[0].properties;
+
+      console.log('New lead data from webhook: ', lead);
+      
+      delete lead.createdate;
+      delete lead.lastmodifieddate;
+
+      lead.company = "Roofs Local";
+
+      const createData = {
+        properties: lead
+      }
+
+      console.log(createData)
+
+      const createContactResponse = await fetch(
+        "https://api.hubapi.com/crm/v3/objects/contacts",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${CONTRACTOR_API_KEY}`,
+          },
+          body: JSON.stringify(createData),
+        }
+      );
+
+      if (!createContactResponse.ok) {
+        const errorBody = await createContactResponse.text();
+        throw new Error(
+          `HubSpot API request to create contact failed with status ${createContactResponse.status}: ${errorBody}`
+        );
+      }
+
+      const createContactResult = await createContactResponse.json();
+      console.log('Contact created in HubSpot: ', createContactResult);
+    } catch (error) {
+      console.error(
+        `Error processing webhook data from HubSpot:`,
+        body.objectId, error
+      );
+    }
+
     res.status(200).json({ message: 'Webhook received successfully' });
   } else {
     res.setHeader('Allow', ['POST']);
