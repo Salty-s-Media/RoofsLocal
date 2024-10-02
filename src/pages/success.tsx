d890adfdfc45ae07352b9ea1e7f25cf7bbef7ffe
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-10-01 18:03:38 -0700

    dont overwriten existing session

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -8,16 +8,20 @@ export default function Success() {
   const [remainingTime, setRemainingTime] = useState(3);
 
   const saveId = async () => {
-    const email = localStorage.getItem("email") as string;
+    // const email = localStorage.getItem("email") as string;
     const urlParams = new URLSearchParams(window.location.search);
     const sessionId = urlParams.get("session_id") || "";
-    await fetch(`/api/user/email/${email}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ sessionId: sessionId }),
-    });
+
+    console.log("Stripe Session ID: ", sessionId);
+
+    // needs to be fixed
+    // await fetch(`/api/user/email/${email}`, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ stripeSessionID: sessionId }),
+    // });
   };
 
   useEffect(() => {
