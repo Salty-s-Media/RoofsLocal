14bd9f64f2e69595818882071e9ffa510122a330
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-10-07 20:12:36 -0700

    cleanup

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -89,7 +89,7 @@ export default function HelpPage() {
             <h3 className="text-xl font-semibold mb-2">Billing Process</h3>
             <p>
               Contractors are charged once a day at 5AM EST for all the leads
-              received for that contractor's zip code that day. The current
+              received for that contractor&quot;s zip code that day. The current
               price per lead is 250 USD, so if Roofs Local sends you 3 leads
               throughout the day, you will be charged 750 USD the next morning.
               We use Stripe for secure and reliable payment and will charge the
@@ -101,7 +101,7 @@ export default function HelpPage() {
             <p>
               It is not a must, but if you would like for leads to come into
               your own hubspot account as soon as we get them, Roofs Local needs
-              your Hubspot key to do that. Don't worry, if you follow the
+              your Hubspot key to do that. Don&quot;t worry, if you follow the
               directions in the tutorial video on the dashboard, the permissions
               will be set so that Roofs Local can only create new contacts, it
               cannot read any data in your Hubspot account such as contacts or
