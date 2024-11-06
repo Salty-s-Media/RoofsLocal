90564c0e202994f725c71ddb5fe0bf6516273e1a
rcong315 <richardcong635@gmail.com>
2024-11-05 22:19:56 -0800

    Fix

--- src/pages/api/cron/charge_users.ts
+++ src/pages/api/cron/charge_users.ts
@@ -4,6 +4,7 @@ import { Resend } from "resend";
 import { Parser } from "json2csv";
 import Stripe from "stripe";
 import twilio from "twilio";
+import { PipelineStage } from "@hubspot/api-client/lib/codegen/crm/pipelines";
 
 const prisma = new PrismaClient();
 const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
@@ -153,6 +154,9 @@ export default async function handler(
         hubspotKey: true,
         ghlKey: true,
         ghlLocationId: true,
+        ghlContactId: true,
+        ghlPipelineId: true,
+        ghlPipelineStageId: true,
       },
     });
 
@@ -270,7 +274,8 @@ export default async function handler(
               }),
             });
             console.log("GHL Response: ", ghlResponse);
-            const ghlResponse3 = await fetch("https://services.leadconnectorhq.com/oportunities/", {
+            const ghlData = await ghlResponse.json();
+            const ghlResponse3 = await fetch("https://services.leadconnectorhq.com/opportunities/", {
               method: "POST",
               headers: {
                 "Content-Type": "application/json",
@@ -279,10 +284,11 @@ export default async function handler(
               },
               body: JSON.stringify({
                 pipelineId: contractor?.ghlPipelineId ? contractor.ghlPipelineId : "",
+                pipelineStageId: contractor?.ghlPipelineStageId ? contractor.ghlPipelineStageId : "",
                 locationId: contractor?.ghlLocationId ? contractor.ghlLocationId : "",
                 name: `${result.properties.firstname} ${ result.properties.lastname}`,
                 status: "open",
-                contactId: (await ghlResponse.json()).contact.id,
+                contactId: ghlData.contact.id,
               }),
             });
       
