c87522a93ddcceb492b85cdb73dbdb8679bb7573
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-11-14 19:49:39 -0800

    allow re-signup upon cancel

--- src/pages/cancel.tsx
+++ src/pages/cancel.tsx
@@ -1,9 +1,35 @@
 "use client";
 
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function CancelPage() {
   const router = useRouter();
+
+  useEffect(() => {
+    const email = localStorage.getItem("email") as string;
+    deleteEmail(email);
+  }, []);
+
+  // Ensure the user can sign up again if they choose to
+  async function deleteEmail(email: string) {
+    const del = await fetch(`/api/user/email/${email}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email }),
+    });
+
+    if (!del.ok) {
+      console.error("HTTP error", del.status);
+      return;
+    }
+
+    const res = await del.json();
+    console.log(res);
+  }
+
   return (
     <div className="min-h-screen w-full justify-center flex flex-col">
       <div className="relative items-center flex flex-col w-full">
