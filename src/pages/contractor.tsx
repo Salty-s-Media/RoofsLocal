a1c08434298ab4f2c9c6f846d86a4da98d47b475
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-11-26 09:18:33 -0800

    loading screen and smaller fixes

--- src/pages/contractor.tsx
+++ src/pages/contractor.tsx
@@ -1,10 +1,11 @@
 import ContractorRegistration from "../../_components/home/contractor/ContractorRegistration";
 export default function Contractor() {
   return (
-    <div className="min-h-screen w-full justify-center flex flex-col my-6 bg-blk">
-      <h1 className="text-center text-3xl font-bold p-6 ">
+    <div className="min-h-screen w-full justify-center pt-6 flex flex-col bg-blk">
+      <h1 className="text-center text-3xl font-bold p-6">
         Sign Up to Buy Leads by Area Code
       </h1>
+      <br></br>
       <ContractorRegistration />
     </div>
   );
