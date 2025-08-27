7a1436f1281a9a1cd59013af702c32b8e4633a2b
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2025-08-26 21:38:33 -0400

    feat: upgrade III

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Roofs Local</title>
+      <meta property="og:title" content="Roof's Local" />
+      <meta
+        property="og:description"
+        content="Providing best in class lead aquistion tools for roofing contractors."
+      />
+      <meta
+        charSet="utf-8"
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      />
+      <meta
+        name="description"
+        content="Providing best in class lead aquistion tools for roofing contractors."
+      />
       <body>
         <Main />
         <NextScript />
